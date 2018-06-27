767c9030aa0314c6be09677dd8d5543cff56e5a4
JMAR <34893438+JMAR-DNY@users.noreply.github.com>
2018-06-27 10:14:46 -0400

    Update kmlgen.php
    added extended data with data fields.  will need to change when using for program service locations.  also need to add commenting

--- kmlgen.php
+++ kmlgen.php
@@ -6,7 +6,7 @@ require 'functions.php';
 
 $conn = Connect();
 
-$stmt = $conn->query('SELECT * FROM fl_zip_coords WHERE 1');
+$stmt = $conn->query('SELECT * FROM org_zip_coords WHERE 1');
 $stmt->setFetchMode(PDO::FETCH_ASSOC);
 
 $doc = new DOMDocument('1.0', 'UTF-8');
@@ -18,25 +18,45 @@ foreach ($stmt as $row) {
       // Creates a Placemark and append it to the Document
     
         $placemark = $doc->createElement('Placemark');
-        $placemark->appendChild($doc->createElement('name', $row['ZipCode']));
-        $placemark->appendChild($doc->createElement('styleUrl', '#msn_ylw-pushpin'));
-
-        $linearRing = $doc->createElement('LinearRing');
-
-        $outerBoundaryIs = $doc->createElement('outerBoundaryIs');
-
-        $polygon = $doc->createElement('Polygon');
-        $polygon->appendChild($doc->createElement('tesselate', 1));
-    //$entry->setAttribute('zipcode', $row['ZipCode']);
-    //[City]
-            
-
         
-    //$test->appendChild($doc->createElement('linear Ring'));
-                $linearRing->appendChild($doc->createElement('Coordinates', $row['Coordinates']));
+        $placemark->appendChild($doc->createElement('name', $row['ZipCode']));
+        
+        $extendedData = $doc->createElement('ExtendedData');
+
+            $data0 = $doc->createElement('Data');               
+            $data0->setAttribute("name","city");//This sets the attribute of the extended data tag
+            $data0->appendChild($doc->createElement('Value', $row['City']));
+            
+            $data1 = $doc->createElement('Data');               
+            $data1->setAttribute("name","org");//This sets the attribute of the extended data tag
+            $data1->appendChild($doc->createElement('Value', $row['org_ID']));
+
+            $data2 = $doc->createElement('Data');               
+            $data2->setAttribute("name","count");//This sets the attribute of the extended data tag
+                $test = (rand(0,100));
+            $data2->appendChild($doc->createElement('Value', $test));
+            /*  
+        $placemark->appendChild($doc->createElement('Data', $row['City']));
+        $placemark->appendChild($doc->createElement('Data', $row['org_ID']));
+        $test = (rand(0,100));
+        $placemark->appendChild($doc->createElement('Data', $test));
+        */
+        $placemark->appendChild($doc->createElement('styleUrl', '#msn_ylw-pushpin'));
+        
+            $linearRing = $doc->createElement('LinearRing');
+
+            $outerBoundaryIs = $doc->createElement('outerBoundaryIs');
+
+            $polygon = $doc->createElement('Polygon');
+            $polygon->appendChild($doc->createElement('tesselate', 1));
+            $linearRing->appendChild($doc->createElement('Coordinates', $row['Coordinates']));
             
             $outerBoundaryIs->appendChild($linearRing);
             $polygon->appendChild($outerBoundaryIs);
+            $extendedData->appendChild($data0);
+            $extendedData->appendChild($data1);
+            $extendedData->appendChild($data2);
+            $placemark->appendChild($extendedData);
             $placemark->appendChild($polygon);
 
            // $outerBoundaryIs->appendChild($linearRing);
@@ -49,5 +69,5 @@ $doc->appendChild($folder);//end folder
 // Set the appropriate content-type header and output the XML
 header('Content-type: application/xml');
 echo $doc->saveXML();
-$doc->save("test1.kml");
+$doc->save("tampa_metro_zip.kml");
 exit;
